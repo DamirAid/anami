742d6c50c9e227bae4008e528bcf2b7315964c33
DamirAid <damir.redmouse@gmail.com>
2018-11-13 23:40:43 +0600

    anam

---
+++ js/main.js
@@ -0,0 +1,28 @@
+	$(".toggle_mnu").click(function() {
+		$(".sandwich").toggleClass("active");
+	});
+
+	$(".top_mnu ul a").click(function() {
+		$(".top_mnu").fadeOut(600);
+		$(".sandwich").toggleClass("active");
+		$(".top_text").css("opacity", "1");
+	}).append("<span>");
+
+	$(".toggle_mnu").click(function() {
+		if ($(".top_mnu").is(":visible")) {
+			$(".top_text").css("opacity", "1");
+			$(".top_mnu").fadeOut(600);
+			$(".top_mnu li a").removeClass("fadeInUp animated");
+		} else {
+			$(".top_text").css("opacity", ".1");
+			$(".top_mnu").fadeIn(600);
+			$(".top_mnu li a").addClass("fadeInUp animated");
+		};
+	});
+
+new WOW().init();
+
+$(".top_mnu ul a").mPageScroll2id();            
+$(window).scroll(function(){
+    $('.top_line').toggleClass('top_line_bg', $(this).scrollTop() > 0);
+});
---
+++ js/slick.js
@@ -0,0 +1,20 @@
+    $('.slider').slick({ 
+      infinite: false,
+      arrows: false,
+      dots: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay:true,      
+    });
+
+    $('.slider2').slick({
+       infinite: false,
+      arrows: false,
+      dots: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay:true, 
+
+    });
+
+    
